1e157b54f95aa801fe8d04c308a214b16f30c3b2
Masayuki Iwai <miyabi@mybdesign.com>
2016-07-10 22:53:02 +0900

    Fix platform condition.

--- Example/Assets/Example.cs
+++ Example/Assets/Example.cs
@@ -1,13 +1,13 @@
 using System.Runtime.InteropServices;
 
 public class Example {
-    #if UNITY_IOS
+    #if UNITY_IOS && !UNITY_EDITOR
     [DllImport("__Internal")]
     private static extern void _ex_callSwiftMethod(string message);
     #endif
 
     public static void CallSwiftMethod(string message) {
-	    #if UNITY_IOS
+	    #if UNITY_IOS && !UNITY_EDITOR
 	    _ex_callSwiftMethod(message);
     	#endif
     }
