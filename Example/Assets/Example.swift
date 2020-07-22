bdae789f680e4bec4a0c616393ad1b83aaecaccf
Yosuke Nakano <ynakano@moff.mobi>
2020-07-22 18:07:49 +0900

    fix swift code

--- Example/Assets/Example.swift
+++ Example/Assets/Example.swift
@@ -10,8 +10,12 @@ import Foundation
 
 class Example : NSObject {
     static func callUnityMethod(_ message: String) {
+        let uf = UnityFramework()
         // Call a method on a specified GameObject.
-        UnitySendMessage("CallbackTarget", "OnCallFromSwift", message)
+        uf.sendMessageToGO(
+            withName: "CallbackTarget",
+            functionName: "OnCallFromSwift",
+            message: message)
     }
     
     @objc static func swiftMethod(_ message: String) {
